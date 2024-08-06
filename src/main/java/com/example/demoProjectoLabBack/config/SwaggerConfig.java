d4234b1c8417c1e337b73fcc809522bd969489d7
Ingbakk16 <ingbakk16@gmail.com>
2024-08-06 15:25:47 -0300

    Swagger Config and Jwt Token

---
+++ src/main/java/com/example/demoProjectoLabBack/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.example.demoProjectoLabBack.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Spring Boot REST API")
+                        .description("Spring Boot REST API Documentation")
+                        .version("1.0.0"))
+                .servers(List.of(new Server().url("http://localhost:8080").description("Local server")));
+    }
+}
---
+++ src/main/java/com/example/demoProjectoLabBack/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,2 @@
+package com.example.demoProjectoLabBack.filter;public class JwtAuthenticationFilter {
+}
